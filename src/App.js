a8bd5244484e305c29cf27bc41f6a9ef2af93388
LeonardoJaques <jaques.projetos@outlook.com>
2021-03-31 15:58:38 -0300

    Refactor App.js for a function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import { ThemeProvider } from './contexts/ThemeContext';
-import React, { Component } from 'react';
+import React from 'react';
 import Form from './Form';
 import Navbar from './Navbar';
 import PageContent from './PageContent';
 import { LanguageProvider } from './contexts/LanguagesContext';
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <Navbar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
